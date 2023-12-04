193ea1652cd8d433704dea93131e389c99fdb3cc
Daerean Legrand <frankrogerstg@gmail.com>
2023-12-04 15:46:48 -0500

    Initial commit

---
+++ CMakeFiles/SpaceInvaders.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/src/glad.c" "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/CMakeFiles/SpaceInvaders.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/ArchivosOpenGL/glfw-3.3.8"
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/ArchivosOpenGL/glfw-3.3.8/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/src/cubes.cpp" "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/CMakeFiles/SpaceInvaders.dir/src/cubes.cpp.o"
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/src/dRays.cpp" "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/CMakeFiles/SpaceInvaders.dir/src/dRays.cpp.o"
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/src/figure.cpp" "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/CMakeFiles/SpaceInvaders.dir/src/figure.cpp.o"
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/src/main.cpp" "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/CMakeFiles/SpaceInvaders.dir/src/main.cpp.o"
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/src/shaderClass.cpp" "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/LABORATORIOS/SpaceInvaders_Proyect/CMakeFiles/SpaceInvaders.dir/src/shaderClass.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/ArchivosOpenGL/glfw-3.3.8"
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/ArchivosOpenGL/glfw-3.3.8/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/angel/Documentos/UCSP/Semestre7/ComputacionGrafica/ArchivosOpenGL/glfw-3.3.8/src/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/SpaceInvaders.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceInvaders.dir/src/cubes.cpp.o"
+  "CMakeFiles/SpaceInvaders.dir/src/dRays.cpp.o"
+  "CMakeFiles/SpaceInvaders.dir/src/figure.cpp.o"
+  "CMakeFiles/SpaceInvaders.dir/src/glad.c.o"
+  "CMakeFiles/SpaceInvaders.dir/src/main.cpp.o"
+  "CMakeFiles/SpaceInvaders.dir/src/shaderClass.cpp.o"
+  "SpaceInvaders"
+  "SpaceInvaders.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/SpaceInvaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
